dd25ad4209056e27606385dc797aba78abb2618f
Javlonbek Shukurov <javlonbeksamdu@gmail.com>
2024-09-21 22:44:18 +0500

    GET method 2 ways

--- GET method  example.py
+++ GET method  example.py
@@ -6,10 +6,12 @@ python_izohli_lugati = {
     'list':"Ro'yxat",
     'tuple':"O'zgarmas ro'yxat"}
 
-
+# one way
 kalit=input("So'zni kiriting:")
 tarjima=python_izohli_lugati.get(kalit,'Bunday lugat mavjud emas')
-print(tarjima)
+#print(tarjima)
+# second way
+print(python_izohli_lugati.get(kalit,"Bunday so'z mavjud emas"))
 
 
 
